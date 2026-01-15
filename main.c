c664afdc5069f065f8450ad0541a9f4d36f97fcd
nikakaladze <nikakaladze89@gmail.com>
2026-01-16 03:27:12 +0400

    practice to C Multidimensional Arrays

--- main.c
+++ main.c
@@ -2,10 +2,15 @@
 
 int main(void)
 {
-char mystr[] ="hello world from C";
-printf("%s",mystr);
+const int my_array[3][3] = {{1,2,3},{4,5,6},7,8,9};
+for(int i = 0;i < 3;i++){
+    for(int j = 0;j < 3;j++){
+        printf("%d ",my_array[i][j]);
+    }
+    
+    printf("\n");
+}
 return 0;
-
 
 }
 
