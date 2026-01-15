c959fc19fd725f2ba7af7ee54deb47ac6d14a5f3
nikakaladze <nikakaladze89@gmail.com>
2026-01-16 03:53:16 +0400

    difference between sizeof and strlen functions

--- strings-functions.c
+++ strings-functions.c
@@ -4,6 +4,8 @@
 int main(void){
     char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     printf("%zu",strlen(alphabet)); // თუ გვინდა რომ გავიგოთ ჩვენი სტრინგის სიგრე ანუ length ვიყენებთ strlen ფუნქციას //
+    printf("%zu",sizeof(alphabet)); // sizeof ოპერატორით გავიგებთ რამდენი ბაიტი უჭირავს მეხსიერებაში ჩვენს სტრინგის   //
+
     return 0;
 
 }   
