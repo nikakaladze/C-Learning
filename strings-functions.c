1b15b164cac8f94c6a4e39f876aec47e4814fe85
nikakaladze <nikakaladze89@gmail.com>
2026-01-16 03:45:58 +0400

    import strlen functions

---
+++ strings-functions.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    printf("%zu",strlen(alphabet)); // თუ გვინდა რომ გავიგოთ ჩვენი სტრინგის სიგრე ანუ length ვიყენებთ strlen ფუნქციას //
+    return 0;
+
+}   
